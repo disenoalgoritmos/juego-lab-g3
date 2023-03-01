160ebf00111cf9696b2315752c7d3dad947c015a
Vallepablo2000 <Pablo.Valle1@alu.uclm.es>
2023-03-01 23:29:29 +0100

    Modifico paso de mensaje JSON

--- client.py
+++ client.py
@@ -1,4 +1,10 @@
 import socket
+import json
+
+def __init__(self):
+    self.user=""
+    self.password=""
+    self.new_password=""
 
 def menu():
     opcion = input("Seleccione una opción: ")
@@ -24,35 +30,36 @@ def menu():
 def login():
     username = input("Ingrese su nombre de usuario: ")
     password = input("Ingrese su contraseña: ")
-    return "LOG_IN (login: " + username + "pass:" + password +" )"
+    return {"Type": "LOG_IN", "user": username , "pass" : password}
 
 def register():
     username = input("Ingrese su nombre de usuario: ")
     password = input("Ingrese su contraseña: ")
-    return "ADD_USER (login: " + username + "pass:" + password +" )"
+    return {"Type" : "ADD_USER", "user" : username,"pass" : password}
 
-def comprobeResponse(message):
+def comprobeResponse(self, message):
     response = client_socket.recv(1024).decode()
-    print(response)
-    if message.starwith("ADD_USER") and response == "OK":
+    if message['Type']== "ADD_USER" and response == "OK":
         print("Usuario registrado correctamente. Puede iniciar sesión")
         message=login()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse(message)
-    elif message.starwith("ADD_USER") and response == "ERROR":
+    elif message['Type']== "ADD_USER" and response == "ERROR":
         print("El usuario ya existe. Intente con otro nombre de usuario")
         message = menu()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse(message)
-    elif message.starwith("LOG_IN") and response == "OK":
+    elif message['Type']== "LOG_IN" and response == "OK":
         print("Bienvenido")
+        self.user=message['user']
+        self.password=message['pass']
         message = menu2()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse2(message)
-    elif message.starwith("LOG_IN") and response == "ERROR":
+    elif message['Type']== "LOG_IN" and response == "ERROR":
         print("Usuario o contraseña incorrectos. Intente de nuevo")
         message = menu()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse(message)
 
 def menu2():
@@ -61,22 +68,22 @@ def menu2():
     print("2. Eliminar mi usuario")
     print("3. Crear una partida")
     print("4. Buscar una partida")
-    print("5. Salir")
+    print("5. Cerrar sesión")
 
     if opcion.isdigit():
         opcion = int(opcion)
         if opcion == 1:
             modifyUser()
         elif opcion == 2:
-            return "DELETE_USER"
+            return {"Type" : "DELETE_USER", "user" : self.user, "pass" : self.password}
         elif opcion == 3:
-            return "NEW_GAME"
+            return {"Type" : "NEW_GAME"}
         elif opcion == 4:
-            return "SEARCH_GAME"
+            return {"Type" : "SEARCH_GAME"}
         elif opcion == 5:
-            return "LOG_OUT"
+            return {"Type" : "LOG_OUT"}
             message = menu()
-            client_socket.send(message.encode())
+            client_socket.send(json.dumps(message).encode())
 
             comprobeResponse(message)
         else:
@@ -86,60 +93,65 @@ def menu2():
         print("Opción inválida, introduzca un número")
         menu2()
 
-def modifyUser():
-    username = input("Ingrese su nuevo nombre de usuario: ")
-    password = input("Ingrese su nueva contraseña: ")
-    return "MODIFY_USER (login: " + username + "pass:" + password +" )"
+def modifyUser(self):
+    new_password = input("Ingrese su nueva contraseña: ")
+    return {"Type" : "MODIFY_USER", "user" : self.user, "pass" : self.password, "new_pass" : new_password}
 
 def comprobeResponse2(message):
     response = client_socket.recv(1024).decode()
     
-    if message.starwith("MODIFY_USER") and response == "OK":
+    if message['Type']=="MODIFY_USER" and response == "OK":
         print("Usuario modificado correctamente")
+        self.password=message['new_pass']
+        self.new_password=""
         message=menu2()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse2(message)
-    elif message.starwith("MODIFY_USER") and response == "ERROR":
-        print("El usuario ya existe. Intente con otro nombre de usuario")
+    elif message['Type']=="MODIFY_USER" and response == "ERROR":
+        print("No se pudo modificar correctamente el usuario. Intente de nuevo")
         message = menu2()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse2(message)
-    elif message.starwith("DELETE_USER") and response == "OK":
+    elif message['Type']=="DELETE_USER" and response == "OK":
         print("Usuario eliminado correctamente")
+        self.user=""
+        self.password=""
         message = menu()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse(message)
-    elif message.starwith("DELETE_USER") and response == "ERROR":
-        print("El usuario no existe")
+    elif message['Type']=="DELETE_USER" and response == "ERROR":
+        print("No se pudo eliminar el usuario. Intente de nuevo")
         message = menu2()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse2(message)
-    elif message.starwith("NEW_GAME") and response != "ERROR":
+    elif message['Type']=="NEW_GAME" and response != "ERROR":
         print("Partida creada correctamente")
         #Partida creada, conexion con game?
-    elif message.starwith("NEW_GAME") and response == "ERROR":
+    elif message['Type']=="NEW_GAME" and response == "ERROR":
         print("No se ha podido crear la partida")
         message = menu2()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse2(message)
-    elif message.starwith("SEARCH_GAME") and response != "ERROR":
+    elif message['Type']=="SEARCH_GAME" and response != "ERROR":
         print("Partida encontrada: " + response)
         #Unirse a una partida
         
-    elif message.starwith("SEARCH_GAME") and response == "ERROR":
+    elif message['Type']=="SEARCH_GAME" and response == "ERROR":
         print("No se ha encontrado ninguna partida, pruebe a crear una nueva")
         message = menu2()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse2(message)
-    elif message.starwith("LOG_OUT") and response == "OK":
+    elif message['Type']=="LOG_OUT" and response == "OK":
         print("Sesión cerrada correctamente")
+        self.user=""
+        self.password=""
         message = menu()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse(message)
-    elif message.starwith("LOG_OUT") and response == "ERROR":
-        print("No se ha podido cerrar la sesión")
+    elif message['Type']=="LOG_OUT" and response == "ERROR":
+        print("No se ha podido cerrar la sesión. Intente de nuevo")
         message = menu2()
-        client_socket.send(message.encode())
+        client_socket.send(json.dumps(message).encode())
         comprobeResponse2(message)
 
 
@@ -150,7 +162,7 @@ if __name__ =="__main__":
     client_socket.connect(server_address)
 
     message = menu()
-    client_socket.send(message.encode())
+    client_socket.send(json.dumps(message).encode())
 
     comprobeResponse(message)
 
