76140a8e684e742beea9b96be76b565e3b9ee0ca
EnriqueAP6 <Enrique.Albalate@alu.uclm.es>
2023-03-02 16:00:18 +0100

    Añadidos métodos bd

--- server_game.py
+++ server_game.py
@@ -1,4 +1,5 @@
 import asyncio
+import threading
 
 
 class servidor:
@@ -51,6 +52,128 @@ class servidor:
 
     '''
 
+    def inicializaBBDD(self, nombre_archivo):
+    
+        #operación necesaria para inicializar el fichero
+        #en caso de no existir previamente
+        base_datos = open(nombre_archivo,"a", encoding="utf-8") # pylint:disable=R1732
+        base_datos.close()
+        #necesitaremos un candado para el archivo de usuarios
+        candado_archivo_texto = threading.Lock()
+        return nombre_archivo,candado_archivo_texto
+
+
+    def inserta_credenciales_archivo(self, user, password, nombre_archivo):
+            '''Añade a la base de datos el usuario y contraseña pasados como parámetros'''
+
+            with open(nombre_archivo,"a", encoding="utf-8") as archivo_escribir:
+                archivo_escribir.write("\n")
+                archivo_escribir.write("[USUARIO] "+ user+"\n")
+                archivo_escribir.write("[CONTRASEÑA] " + password+"\n")
+                archivo_escribir.close()
+            print(f"Añadidas las credenciales de: {user}\n")
+
+
+    def busca_user_archivo(self, user, nombre_archivo, candado_archivo_texto):
+            """Devuelve la línea en la que está escrito, dentro del archivo"""
+            """ de texto que se usa como base de datos, el nombre de un usuario buscado""" # pylint:disable=W0105
+
+            with candado_archivo_texto:
+                #controlo el archivo persistente con sección crítica
+
+                with open(nombre_archivo,"r", encoding="utf-8") as archivo_leer:
+                    lineas_leidas = archivo_leer.readlines()
+                    archivo_leer.close()
+
+                for num_linea in range(len(lineas_leidas)): # pylint:disable=C0200
+                    if user == lineas_leidas[num_linea][len("[USUARIO] "):-1]:
+                        #[:-1] para quitar el salto de línea
+                        return num_linea
+
+            #elimina los errores que pudieran producirse si se intenta eliminar
+            # un user que no existe
+            return -1
+
+
+    def comprueba_credenciales(self, user, password, nombre_archivo, candado_archivo_texto):
+            """Verifica que un usuario y contraseña estén en el """
+            """arhivo de texto usado como base de datos""" # pylint:disable=W0105
+            credenciales_validas = False
+
+            with candado_archivo_texto:
+                #controlo el archivo persistente con sección crítica
+
+                with  open(nombre_archivo,"r",encoding="utf-8") as archivo_leer:
+                    lineas_leidas = archivo_leer.readlines()
+                    archivo_leer.close()
+
+                for num_linea in range(len(lineas_leidas)): # pylint:disable=C0200
+                    if user == lineas_leidas[num_linea][len("[USUARIO] "):-1]:
+                        if password == lineas_leidas[num_linea+1][len("[CONTRASEÑA] "):-1]:
+                        #primera condición evita que falle porque la última contraseña sea igual al
+                        # nombre del usuario buscado
+                        #tercera condición permite comprobar si un usuario ya está registrado
+                        # en la base de datos
+                            credenciales_validas = True
+
+            return credenciales_validas
+
+
+    def elimina_lineas_archivo(self, num_linea, nombre_archivo, candado_archivo_texto):
+            """Reescribe el contenido del archivo de texto usado como"""
+            """base de datos saltándose ciertsa líneas""" # pylint:disable=W0105
+
+            with candado_archivo_texto:
+                #controlo el archivo persistente con sección crítica
+
+                with open(nombre_archivo,"r", encoding="utf-8") as archivo_leer:
+                    lineas_leidas = archivo_leer.readlines()
+                    archivo_leer.close()
+
+                with open(nombre_archivo,"w", encoding="utf-8") as archivo_leer:
+
+                    for i in range(len(lineas_leidas)): # pylint:disable=C0200
+                        if i not in [num_linea,num_linea + 1]: #se salta las líneas que contienen
+                        #las credenciales a borrar
+                            archivo_leer.write(lineas_leidas[i])
+                            #ahora no añado el salto de línea porque
+                            #dejaría una línea en blanco entre datos
+                    archivo_leer.close()
+
+
+    def addUser(self,user, passwordHash, nombre_archivo, candado_archivo_texto):
+        "Función administrativa que permite añadir unas nuevas credenciales en el almacén de datos"
+        "si el token administrativo es correcto" # pylint:disable=W0105
+
+        if self.busca_user_archivo(user,nombre_archivo,candado_archivo_texto) == -1 :
+        #controla el caso de querer registrar un usuario que ya existe
+        #(basta con que el nombre del usuario esté en la BD)
+            with candado_archivo_texto:
+                #controlo el archico de credenciales de usuario con sección crítica
+                self.inserta_credenciales_archivo(user,passwordHash,nombre_archivo)
+
+            
+    def removeUser(self, user, passwordHash, nombre_archivo, candado_archivo_texto):
+            "Función administrativa que permite eliminar unas credenciales" 
+
+            if self.comprueba_credenciales(user,passwordHash,nombre_archivo,candado_archivo_texto):
+                posicion_user_archivo = self.busca_user_archivo(user,nombre_archivo,candado_archivo_texto)
+
+                if  posicion_user_archivo != -1: #controla el caso de que se intente
+                #borrar un user inexistente
+
+                    #hay que borrar al user tanto del archivo ...
+                    self.elimina_lineas_archivo(posicion_user_archivo,nombre_archivo,candado_archivo_texto)
+                    print(f"Eliminadas las credenciales de: {user}\n")
+            #else:
+                #habría que devolver un error o algo así
+
+    def modifyUser(self, user, passwordHash, new_password, nombre_archivo, candado_archivo_texto):
+        if self.comprueba_credenciales(user,passwordHash,nombre_archivo,candado_archivo_texto):
+            self.removeUser(user,passwordHash,nombre_archivo,candado_archivo_texto)
+            self.addUser(user,new_password,nombre_archivo,candado_archivo_texto)
+            print(f"Modificadas las credenciales de: {user}\n")
+
 
 async def handle_echo(reader, writer):
     data = await reader.read(100)
@@ -78,3 +201,17 @@ async def main():
         await server.serve_forever()
 
 asyncio.run(main())
+
+#server = servidor()
+#nombre_archivo,candado_archivo_texto = server.inicializaBBDD("credenciales.txt")
+
+#print(server.comprueba_credenciales("Enrique","hola",nombre_archivo,candado_archivo_texto))
+#server.addUser("Enrique","hola",nombre_archivo,candado_archivo_texto)
+#print(server.comprueba_credenciales("Enrique","hola",nombre_archivo,candado_archivo_texto))
+
+#server.modifyUser("Enrique","hola","adios",nombre_archivo,candado_archivo_texto)
+#print(server.comprueba_credenciales("Enrique","hola",nombre_archivo,candado_archivo_texto))
+#print(server.comprueba_credenciales("Enrique","adios",nombre_archivo,candado_archivo_texto))
+
+#server.removeUser("Enrique","hola",nombre_archivo,candado_archivo_texto)
+#print(server.comprueba_credenciales("Enrique","adios",nombre_archivo,candado_archivo_texto))
