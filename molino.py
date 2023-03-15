067a9dc2ff92cb4388418dcd3031e932bcaa5a52
IGNACIO ROZAS LÓPEZ <Ignacio.Rozas@alu.uclm.es>
2023-03-15 17:17:21 +0100

    Algoritmo Minimax

--- molino.py
+++ molino.py
@@ -1,12 +1,14 @@
 import random
 import json
 import copy
+import sys
 class Estado:
     def __init__(self):
         self.Free = [True] *24
         self.Gamer = [[],[]]
         self.Turn = 0
         self.chips = 18
+        self.valor = None
        
     def to_json(self):
         estado_dict = {
@@ -29,7 +31,7 @@ class Estado:
             
             for indice, casillaLibre in enumerate(self.Free):                        
                 if casillaLibre==True: # para cada posicion libre disponible
-                    if tablero.comprobarMolino(indice):#Si se ha formado un molino, se busca todas las fichas comibles
+                    if tablero.comprobarMolino(indice,origen):#Si se ha formado un molino, se busca todas las fichas comibles
                         fichasComibles = tablero.FichasComibles()                        
                         for ficha in fichasComibles:
                             yield Accion(origen,indice,ficha)
@@ -40,7 +42,7 @@ class Estado:
             for origen in self.Gamer[self.Turn]:
                 #para cada ficha del jugador con turno
                 for destino in self.MovimientosValidos(origen):
-                    if self.comprobarMolino(destino):
+                    if self.comprobarMolino(destino,origen):
                         fichasComibles = self.FichasComibles()
                         for ficha in fichasComibles:
                             yield Accion(origen,destino,ficha)
@@ -110,7 +112,7 @@ class Tablero:
 
  
     def validarAccionKill(self,accion:Accion):
-        if self.comprobarMolino(accion.destino) == True:
+        if self.comprobarMolino(accion.destino,accion.origen) == True:
                 if accion.kill == -1:
                     return False
 
@@ -138,7 +140,7 @@ class Tablero:
         #self.Print()
         self.estado.Turn = (self.estado.Turn+1)%2
 
-    def comprobarMolino(self,posicion):
+    def comprobarMolino(self,posicion,posicion_anterior):
         anillo = self.NivelAnillo(posicion)
         #si la posicion a mirar esta libre entonces se comprueba si se formaria un molino del propio jugador con turno
         if self.estado.Free[posicion] == True:
@@ -149,36 +151,44 @@ class Tablero:
                 turno = 0
             else:#Suponemos que si no es del jugador 0, debe de serlo del jugador 1 ya que esa posicion no esta libre
                 turno = 1
+
+        
         if self.EsPasillo(posicion): #Estamos en un pasillo
 
             if(anillo==0):
                 #Los not free son rendundantes, si esta en Gamer no deberian estar libres
                 if posicion+8 in self.estado.Gamer[turno] and not self.estado.Free[posicion+8] \
                 and posicion+16 in self.estado.Gamer[turno] and not self.estado.Free[posicion+16]:
-                    return True
+                    if posicion_anterior == -1 or (posicion_anterior != posicion+8 and posicion_anterior != posicion+16):
+                        return True
 
             elif anillo==1:
                 if posicion+8 in self.estado.Gamer[turno] and not self.estado.Free[posicion+8] \
                     and posicion-8 in self.estado.Gamer[turno] and not self.estado.Free[posicion-8]:
-                    return True
+                    if posicion_anterior == -1 or (posicion_anterior != posicion+8 and posicion_anterior != posicion-8):
+                        return True
 
             elif anillo==2:
                 if posicion-8 in self.estado.Gamer[turno] and not self.estado.Free[posicion-8] \
                     and posicion-16 in self.estado.Gamer[turno] and not self.estado.Free[posicion-16]:
-                    return True
+                    if posicion_anterior == -1 or (posicion_anterior != posicion-8 and posicion_anterior != posicion-16):
+                        return True
 
             
             elif self.Mover(posicion,1) in self.estado.Gamer[turno] and not self.estado.Free[self.Mover(posicion,1)] \
                 and self.Mover(posicion,-1) in self.estado.Gamer[turno] and not self.estado.Free[self.Mover(posicion,-1)]:
-                return True
+                if posicion_anterior == -1 or (posicion_anterior != self.Mover(posicion,1) and posicion_anterior != self.Mover(posicion,-1)):
+                    return True
 
         else: #estamos en una esquina
             if self.Mover(posicion,1) in self.estado.Gamer[turno] and not self.estado.Free[self.Mover(posicion,1)] \
                 and self.Mover(posicion,2) in self.estado.Gamer[turno] and not self.estado.Free[self.Mover(posicion,2)]:
-                return True
+                if posicion_anterior == -1 or (posicion_anterior != self.Mover(posicion,1) and posicion_anterior != self.Mover(posicion,2)): 
+                    return True
             elif self.Mover(posicion,-1) in self.estado.Gamer[turno] and not self.estado.Free[self.Mover(posicion,-1)] \
                 and self.Mover(posicion,-2) in self.estado.Gamer[turno]and not self.estado.Free[self.Mover(posicion,-2)]:
-                return True
+                if posicion_anterior == -1 or (posicion_anterior != self.Mover(posicion,-1)  and posicion_anterior != self.Mover(posicion,-2)): 
+                    return True
 
         return False
 
@@ -219,7 +229,7 @@ class Tablero:
         fichasComibles = self.estado.Gamer[(self.estado.Turn+1)%2].copy()
         fichasValidas = []
         for ficha in fichasComibles:
-            if not self.comprobarMolino(ficha):
+            if not self.comprobarMolino(ficha,-1):#creo que se puede considerar aqui que no hay origen
                 fichasValidas.append(ficha)
         if len(fichasValidas) == 0:                                    
             #No existen fichas fuera de un molino por lo que todas son comibles
@@ -235,7 +245,7 @@ class Tablero:
             
             for indice, casillaLibre in enumerate(self.estado.Free):                        
                 if casillaLibre==True: # para cada posicion libre disponible
-                    if self.comprobarMolino(indice):#Si se ha formado un molino, se busca todas las fichas comibles
+                    if self.comprobarMolino(indice,origen):#Si se ha formado un molino, se busca todas las fichas comibles
                         fichasComibles = self.FichasComibles()
                         for ficha in fichasComibles:
                             acciones.append(Accion(origen,indice,ficha))
@@ -246,7 +256,7 @@ class Tablero:
             for origen in self.estado.Gamer[self.estado.Turn]:
                 #para cada ficha del jugador con turno
                 for destino in self.MovimientosValidos(origen):
-                    if self.comprobarMolino(destino):
+                    if self.comprobarMolino(destino,origen):
                         fichasComibles = self.FichasComibles()
                         for ficha in fichasComibles:
                             acciones.append(Accion(origen,destino,ficha))
@@ -317,6 +327,86 @@ def testGeneradorAcciones(tablero:Tablero):
         print("No hay mas elementos en el generador")
 
 
+#Le he añadido a la clase estado un nuevo atributo llamado valor para manejar mejor el algoritmo
+#En las tres funciones en lugar de devolver unicamente un estado podria devolver una tupla (estado,valor) y asi quizas no
+#   haria falta modificar los estados
+def miniMax(estado,profundidad):
+    bestAccion = None
+    bestValor = -99999
+    tablero = Tablero(copy.deepcopy(estado))
+    #Recorremos los sucesores
+    for accion in tablero.Acciones():
+        tablero2 = Tablero(copy.deepcopy(estado))
+        tablero2.ejecutarAccion(accion)
+        estadoSucesor = tablero2.estado  #Obtenemos un estado sucesor
+        estadoSucesor.valor = bestValor #Y le asignamos el mejor valor actual
+        sucesorMin = minValor(estadoSucesor,profundidad-1) #Pasamos al siguiente turno y obtenemos el valor minimo del estado
+        if bestValor <= sucesorMin.valor:#Cojemos el valor Maximo entre bestValor y el valor del estado sucesor del minValor
+            bestAccion = accion #Almacenamos la posible mejor accion encontrada
+            bestValor = sucesorMin.valor #Almacenamos el mejor valor actual
+    return bestAccion #Devuelve la mejor accion
+
+def maxValor(estado,profundidad):
+    tablero = Tablero(copy.deepcopy(estado))
+    if profundidad <=0:#limite de profundidad
+        #Asignamos un valor segun la diferencia de fichas entre los jugadores
+        estado.valor = len(estado.Gamer[estado.Turn]) - len(estado.Gamer[(estado.Turn+1)%2])
+        return estado
+    if(tablero.is_end()):#Partida terminada
+        #estamos en un nodo max, asique el turno actual es el que queremos maximizar su valor
+        if(len(estado.Gamer[estado.Turn])>len(estado.Gamer[(estado.Turn+1)%2])):
+            estado.valor = 100#Un valor alto arbitrario
+            return estado
+        else:
+            estado.valor = -100
+            #estado.valor = len(estado.Gamer[estado.Turn]) - len(estado.Gamer[(estado.Turn+1)%2])
+            return estado
+
+    valor = -99999
+    tablero = Tablero(copy.deepcopy(estado))
+    #Recorremos los sucesores
+    for accion in tablero.Acciones():
+        tablero2 = Tablero(copy.deepcopy(estado))
+        tablero2.ejecutarAccion(accion)        
+        estadoSucesor = tablero2.estado #Obtenemos un estado sucesor
+        sucesorMin = minValor(estadoSucesor,profundidad-1)#Pasamos al siguiente turno y obtenemos el valor minimo del estado
+        sucesor = sucesorMin
+        if valor <= sucesorMin.valor:#Cojemos el valor Maximo entre el valor actual del estado y el obtenido en minValor
+            valor = sucesorMin.valor
+            sucesor = sucesorMin
+            sucesor.valor = valor
+    return sucesor
+
+def minValor(estado,profundidad):
+    if profundidad <=0:
+        #Asignamos un valor segun la diferencia de fichas entre los jugadores. En negativo porque es el turno del jugador MIN
+        estado.valor = -(len(estado.Gamer[estado.Turn]) - len(estado.Gamer[(estado.Turn+1)%2]))
+        return estado
+    tablero = Tablero(copy.deepcopy(estado))
+    if(tablero.is_end()):
+        if(len(estado.Gamer[estado.Turn])> len(estado.Gamer[(estado.Turn+1)%2])):
+            estado.valor = -100 #El jugador MIN gana. En negativo porque es el turno del jugador MIN
+            return estado
+        else:
+            estado.valor = 100            
+            # estado.valor = -(len(estado.Gamer[estado.Turn]) - len(estado.Gamer[(estado.Turn+1)%2]))
+            return estado
+
+    valor = 99999
+    tablero = Tablero(copy.deepcopy(estado))
+    #recorremos los sucesores
+    for accion in tablero.Acciones():
+        tablero2 = Tablero(copy.deepcopy(estado))
+        tablero2.ejecutarAccion(accion)
+        estadoSucesor = tablero2.estado  #Obtenemos un estado sucesor
+        sucesorMax = maxValor(estadoSucesor,profundidad-1)#Pasamos al siguiente turno y obtenemos el valor maximo del estado
+        sucesor = sucesorMax
+        if valor >= sucesorMax.valor:#Cojemos el valor Minimo entre el valor actual del estado y el obtenido en maxValor
+            valor = sucesorMax.valor
+            sucesor = sucesorMax
+            sucesor.valor = valor
+    return sucesor
+
 
 
 def getAccion():
@@ -326,19 +416,34 @@ def getAccion():
     return Accion(origen,destino,kill)
 
 if __name__ =="__main__":
-    for i in range(1,100):
+    puntuacion = [0,0]
+    for i in range(1,101):
         tablero = Tablero(Estado())#tablero y estado inicial
         while not tablero.is_end():
             tablero.Print()
             #accion = getAccion()
-            accion = random.choice(tablero.Acciones())     
-            print("json")
-            print(tablero.sucesor(accion).to_json())
+            if tablero.estado.Turn ==1:
+                accion = miniMax(tablero.estado,3)
+                #tablero.estado = estadoMinimax
+            else:
+                accion = random.choice(tablero.Acciones())     
             if tablero.validarAccion(accion): 
                 tablero.ejecutarAccion(accion)
             else:
                 print("Accion no valida")
+            #print("Accion")
+            print(accion.to_json())
+            #print(tablero.sucesor(accion).to_json())
         tablero.Print()
 
         print ("partida "+str(i)+" terminada")
+        if( len(tablero.estado.Gamer[0])<3):
+            print("Ha ganado el Jugador2")
+            puntuacion[1] +=1
+        else:
+            print("Ha ganado el Jugador1")
+            puntuacion[0] +=1
+
+
     print("Todas las partidas terminadas")
+    print(f"Puntuaciones: {puntuacion}")
